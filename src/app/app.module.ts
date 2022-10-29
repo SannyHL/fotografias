66ebf05eae7425701f9e9ce213543cdb3ab3fc41
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-29 18:15:44 -0300

    Exclusão de declarações e imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,13 @@ import { AppComponent } from './app.component';
 import { FotosGestacaoComponent } from './fotos-gestacao/fotos-gestacao.component';
 import { FotosChaBebeComponent } from './fotos-cha-bebe/fotos-cha-bebe.component';
 import { Fotos2019Component } from './fotos2019/fotos2019.component';
-import { Fotos2020Component } from './fotos2020/fotos2020.component';
-import { Fotos2021Component } from './fotos2021/fotos2021.component';
-import { Fotos2022Component } from './fotos2022/fotos2022.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FotosGestacaoComponent,
     FotosChaBebeComponent,
-    Fotos2019Component,
-    Fotos2020Component,
-    Fotos2021Component,
-    Fotos2022Component
+    Fotos2019Component
   ],
   imports: [
     BrowserModule,
